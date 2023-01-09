32d0c3d23120c9056767d2068cb7b81fe01c078a
hà huy hùng <83862705+hahuyhungdev@users.noreply.github.com>
2023-01-09 14:07:18 +0700

    fix: refactor layout content

--- src/pages/devices/index.jsx
+++ src/pages/devices/index.jsx
@@ -1,10 +1,20 @@
 import './style.scss';
 
+import { Input } from 'antd';
+import { ButtonCustom } from 'components/Button';
+import DevicesTable from 'components/Table/Devices';
 import React from 'react';
 export const Devices = () => {
   return (
-    <div className="report">
-      <title>Devices</title>
+    <div className="mainDevices">
+      <header>
+        <title>Objects List</title>
+        <div className="toolbar">
+          <Input placeholder="Search" />
+          <ButtonCustom>Add Object</ButtonCustom>
+        </div>
+      </header>
+      <DevicesTable />
     </div>
   );
 };

c0b4bbf120f941078f8f504844bca67930d8da0f
hà huy hùng <83862705+hahuyhungdev@users.noreply.github.com>
2023-01-12 09:58:35 +0700

    test thu

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -20,6 +20,7 @@ export function Page({ className, pageSize = 10, current, total, onChange }) {
     </div>
   );
 }
+//duasdhkasjdhkajs
 Page.propTypes = {
   pageSize: PropTypes.number,
   current: PropTypes.number,
